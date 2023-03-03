42672add9e24de3dae8d54f93876ef8e161c96be
Kardy <ksm@mailu.com>
2023-03-03 16:53:55 -0500

    updating formulae for error free results

--- app/src/main/java/com/example/kotlinscientificcalculator/MainActivity.kt
+++ app/src/main/java/com/example/kotlinscientificcalculator/MainActivity.kt
@@ -59,19 +59,39 @@ class MainActivity : AppCompatActivity() {
         }
         binding.buttonMinus.setOnClickListener {
             binding.inputView.append("-")
+            val str = binding.inputView.toString()
+            if (!str.get(index = str.length - 1).equals("-")) {
+                binding.inputView.text = (binding.inputView.toString() + "-")
+            }
         }
+
         binding.buttonMultiply.setOnClickListener {
             binding.inputView.append("*")
+            val str = binding.inputView.toString()
+            if (!str.get(index = str.length - 1).equals("*")) {
+                binding.inputView.text = (binding.inputView.toString() + "*")
+            }
         }
+
         binding.buttonDiv.setOnClickListener {
             binding.inputView.append("/")
         }
         binding.buttonDivbyX.setOnClickListener {
             binding.inputView.append("div by")
         }
+
         binding.buttonFactorial.setOnClickListener {
-            binding.inputView.append("!")
+            var str: String = binding.inputView.text.toString()
+            binding.inputView.text = "Fact" + str
+            if (str.isEmpty()) {
+                Toast.makeText(this, "Please enter a valid number first", Toast.LENGTH_SHORT)
+                    .show()
+            } else {
+                var number: Int = str.toInt()
+                binding.outputView.text = factorial(number).toString()
+            }
         }
+
         binding.buttonDot.setOnClickListener {
             binding.inputView.append(".")
         }
@@ -81,61 +101,81 @@ class MainActivity : AppCompatActivity() {
         binding.buttonBraceClose.setOnClickListener {
             binding.inputView.append(")")
         }
+
         binding.buttonSquare.setOnClickListener {
-            binding.inputView.append("square")
-        }
-        binding.buttonSqrt.setOnClickListener {
-            binding.inputView.append("sqrt")
-        }
-        binding.buttonSin.setOnClickListener {
-            binding.inputView.append("Sin")
-        }
-        binding.buttonCos.setOnClickListener {
-            binding.inputView.append("Cos")
-        }
-        binding.buttonTan.setOnClickListener {
-            binding.inputView.append("tan")
-        }
-        binding.buttonLn.setOnClickListener {
-            binding.inputView.append("ln")
-        }
-        binding.buttonLog.setOnClickListener {
-            binding.inputView.append("Log")
-        }
-        binding.buttonPie.setOnClickListener {
-            binding.inputView.append("3.142")
-        }
-        binding.buttonC.setOnClickListener {
-            Toast.makeText(this,"should remove last entered character",Toast.LENGTH_SHORT).show()
-            //binding.inputView.remove character
-        }
-
-
-        binding.buttonEquals.setOnClickListener {
-
-            var expression = ExpressionBuilder(binding.inputView.text.toString()).build()
-            var result = expression.evaluate()
-            var longResult = result.toLong()
-
-            if(result == longResult.toDouble()) {
-                binding.outputView.text = longResult.toString()
+            var str: String = binding.inputView.text.toString()
+            binding.inputView.text = "square" + str
+            if (str.isEmpty()) {
+                Toast.makeText(this, "Please enter a valid number first", Toast.LENGTH_SHORT)
+                    .show()
             } else {
-                binding.outputView.text = result.toString()
+                var square: Double = str.toDouble() * str.toDouble()
+                binding.outputView.text = square.toString()
             }
         }
 
 
+        binding.buttonSqrt.setOnClickListener {
+            var str = binding.inputView.toString()
+            if (str.isEmpty()) {
+                Toast.makeText(this, "Please enter a valid number first", Toast.LENGTH_SHORT)
+                    .show()
+            } else {
+                binding.inputView.text = "Sqrt" + str
+                var sqroot = Math.sqrt(str.toDouble())
+                binding.outputView.text = sqroot.toString()
+            }
+
+
+            binding.buttonSin.setOnClickListener {
+                binding.inputView.append("Sin")
+            }
+            binding.buttonCos.setOnClickListener {
+                binding.inputView.append("Cos")
+            }
+            binding.buttonTan.setOnClickListener {
+                binding.inputView.append("tan")
+            }
+            binding.buttonLn.setOnClickListener {
+                binding.inputView.append("ln")
+            }
+            binding.buttonLog.setOnClickListener {
+                binding.inputView.append("Log")
+            }
+            binding.buttonPie.setOnClickListener {
+                binding.inputView.append("3.142")
+            }
+            binding.buttonC.setOnClickListener {
+                var str: String = binding.inputView.text.toString()
+                if (str.isEmpty()) {
+                    Toast.makeText(this, "Please enter a valid number", Toast.LENGTH_SHORT).show()
+                } else {
+                    str = str.dropLast(1)
+                    binding.inputView.text = str
+                }
+            }
+
+
+            binding.buttonEquals.setOnClickListener {
+
+                var expression = ExpressionBuilder(binding.inputView.text.toString()).build()
+                var result = expression.evaluate()
+                var longResult = result.toLong()
+
+                if (result == longResult.toDouble()) {
+                    binding.outputView.text = longResult.toString()
+                } else {
+                    binding.outputView.text = result.toString()
+                }
+            }
+        }
     }
 
-
-    private enum class Operators(val sign: Char) {
-        MINUS('-'),
-        PLUS('+'),
-        MULTIPLY('*'),
-        DIVISION('/'),
-        POWER('^'),
-        EXPONENTIAL('E');
+    fun factorial(num: Int): Int {
+        return if (num == 1 || num == 0) 1 else num * factorial(num - 1)
+        //using regression function
     }
 }
+
 
 //TODO - animations to button presses

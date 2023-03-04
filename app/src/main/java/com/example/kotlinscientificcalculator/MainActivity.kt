ae55426e9fd9ecb3c59178ac07f101c2c57ba4a0
Kardy <ksm@mailu.com>
2023-03-04 12:25:44 -0500

    bug fixes and performance improvements

--- app/src/main/java/com/example/kotlinscientificcalculator/MainActivity.kt
+++ app/src/main/java/com/example/kotlinscientificcalculator/MainActivity.kt
@@ -1,13 +1,10 @@
 package com.example.kotlinscientificcalculator
 
-import android.annotation.SuppressLint
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.example.kotlinscientificcalculator.databinding.ActivityMainBinding
-import net.objecthunter.exp4j.Expression
-import net.objecthunter.exp4j.ExpressionBuilder
+import kotlin.math.sqrt
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
@@ -25,7 +22,6 @@ class MainActivity : AppCompatActivity() {
         binding.buttonZero.setOnClickListener {
             binding.inputView.append("0")
         }
-
         binding.buttonOne.setOnClickListener {
             binding.inputView.append("1")
         }
@@ -57,17 +53,20 @@ class MainActivity : AppCompatActivity() {
         binding.buttonPlus.setOnClickListener {
             binding.inputView.append("+")
         }
+
         binding.buttonMinus.setOnClickListener {
-            binding.inputView.append("-")
+//            binding.inputView.append("-")
             val str = binding.inputView.toString()
+            //checking if user already entered '-' symbol, not allowing user to add '-' symbol again
             if (!str.get(index = str.length - 1).equals("-")) {
-                binding.inputView.text = (binding.inputView.toString() + "-")
+                binding.inputView.text = (binding.inputView.text.toString() + "-")
             }
         }
 
         binding.buttonMultiply.setOnClickListener {
             binding.inputView.append("*")
             val str = binding.inputView.toString()
+            //checking if user already entered '*' symbol
             if (!str.get(index = str.length - 1).equals("*")) {
                 binding.inputView.text = (binding.inputView.toString() + "*")
             }
@@ -82,12 +81,12 @@ class MainActivity : AppCompatActivity() {
 
         binding.buttonFactorial.setOnClickListener {
             var str: String = binding.inputView.text.toString()
-            binding.inputView.text = "Fact" + str
             if (str.isEmpty()) {
-                Toast.makeText(this, "Please enter a valid number first", Toast.LENGTH_SHORT)
+                Toast.makeText(this, "Enter a valid number", Toast.LENGTH_SHORT)
                     .show()
             } else {
                 var number: Int = str.toInt()
+                binding.inputView.text = "Fact" + str
                 binding.outputView.text = factorial(number).toString()
             }
         }
@@ -104,25 +103,29 @@ class MainActivity : AppCompatActivity() {
 
         binding.buttonSquare.setOnClickListener {
             var str: String = binding.inputView.text.toString()
-            binding.inputView.text = "square" + str
             if (str.isEmpty()) {
-                Toast.makeText(this, "Please enter a valid number first", Toast.LENGTH_SHORT)
+                Toast.makeText(this, "Enter a valid number", Toast.LENGTH_SHORT)
                     .show()
             } else {
-                var square: Double = str.toDouble() * str.toDouble()
+                var double: Double = str.toDouble()
+                var square: Double = double * double
+                binding.inputView.text = "square" + str
                 binding.outputView.text = square.toString()
             }
         }
 
 
         binding.buttonSqrt.setOnClickListener {
-            var str = binding.inputView.toString()
+            //TODO - Does not work
+            var str = binding.inputView.text.toString()
+            //Check if inputView is empty, display toast
             if (str.isEmpty()) {
-                Toast.makeText(this, "Please enter a valid number first", Toast.LENGTH_SHORT)
+                Toast.makeText(this, "Enter a valid number", Toast.LENGTH_SHORT)
                     .show()
             } else {
-                binding.inputView.text = "Sqrt" + str
-                var sqroot = Math.sqrt(str.toDouble())
+                binding.inputView.text = "√" + str
+                //calculating square root using Kotlin fun
+                var sqroot = sqrt(str.toDouble())
                 binding.outputView.text = sqroot.toString()
             }
 
@@ -150,6 +153,7 @@ class MainActivity : AppCompatActivity() {
                 if (str.isEmpty()) {
                     Toast.makeText(this, "Please enter a valid number", Toast.LENGTH_SHORT).show()
                 } else {
+                    //dropping the last entered digit
                     str = str.dropLast(1)
                     binding.inputView.text = str
                 }
@@ -157,25 +161,43 @@ class MainActivity : AppCompatActivity() {
 
 
             binding.buttonEquals.setOnClickListener {
-
-                var expression = ExpressionBuilder(binding.inputView.text.toString()).build()
-                var result = expression.evaluate()
-                var longResult = result.toLong()
-
-                if (result == longResult.toDouble()) {
-                    binding.outputView.text = longResult.toString()
-                } else {
-                    binding.outputView.text = result.toString()
-                }
+                var str: String = binding.inputView.text.toString()
+          //      var result: Double = calculate(str)
+          //      binding.outputView.text = result.toString()
+                binding.inputView.text = ""
             }
         }
     }
 
-    fun factorial(num: Int): Int {
-        return if (num == 1 || num == 0) 1 else num * factorial(num - 1)
-        //using regression function
+    fun calculate(str: String) {
+        //   var result =
+        // return object : Any() {
+        var position = -1
+        var char = 0
+
+        fun nextChar() {
+            //traversing char by char
+            char = if (++position < str.length) str[position].toInt() else -1
+        }
+
+        fun eat(charToEdit: Int): Boolean {
+            //            while (char == ''.toInt().nextChar())
+            //       }
+            //checking extra spacing
+            return true
+        }
     }
-}
+
+    private fun factorial(num: Int): Long {
+        //using recursion function
+        return if (num >= 1) {
+            return num * factorial(num - 1)
+        } else {
+                return 1
+            }
+        }
+    }
+
 
 
 //TODO - animations to button presses
